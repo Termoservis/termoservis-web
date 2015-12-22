8ba501f782203787d7318e199691f88c882b5619
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2015-12-22 03:12:18 +0100

    Initial

---
+++ src/Termoservis/src/Web/Areas/Portal/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+using Web.Areas.Portal.ViewModels.Portal;
+using Web.Models;
+
+namespace Web.Areas.Portal.Controllers
+{
+	/// <summary>
+	/// Portal home controller.
+	/// </summary>
+	[Area("Portal")]
+	[Authorize]
+	public class HomeController : Controller
+	{
+		private readonly IApplicationDbContext context;
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="HomeController"/> class.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		// ReSharper disable once SuggestBaseTypeForParameter
+		public HomeController(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+
+
+		// GET: /portal/home/		
+		/// <summary>
+		/// Overview of portal.
+		/// </summary>
+		/// <returns>Returns the portal home view.</returns>
+		public async Task<IActionResult> Index()
+		{
+			// Retrieve current user
+			var currentUser = await this.context.GetCurrentUserAsync(this);
+
+			// Construct view model
+			var model = new LayoutViewModel(currentUser);
+
+			// Return view 
+			return View(model);
+		}
+	}
+}
---
+++ src/Termoservis/src/Web/Areas/Portal/Controllers/UsersController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Mvc;
+using Web.Areas.Portal.ViewModels.Portal;
+using Web.Models;
+
+namespace Web.Areas.Portal.Controllers
+{
+	/// <summary>
+	/// Portal user management controller.
+	/// </summary>
+	[Area("Portal")]
+	[Authorize]
+    public class UsersController : Controller
+	{
+		private readonly IApplicationDbContext context;
+
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UsersController"/> class.
+		/// </summary>
+		/// <param name="context">The context.</param>
+		// ReSharper disable once SuggestBaseTypeForParameter
+		public UsersController(ApplicationDbContext context)
+		{
+			this.context = context;
+		}
+
+
+		// GET: /portal/users/		
+		/// <summary>
+		/// Overview of portal users.
+		/// </summary>
+		/// <returns>Returns the portal users view.</returns>
+		public async Task<IActionResult> Index()
+		{
+			// Retrieve current user
+			var currentUser = await this.context.GetCurrentUserAsync(this);
+
+			// Construct view model
+			var model = new LayoutViewModel(currentUser);
+
+			// Return view 
+			return View(model);
+		}
+	}
+}
