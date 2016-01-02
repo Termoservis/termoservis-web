4a2d43af456adc55c8d505c74f260f1543d43b53
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2016-01-02 18:02:51 +0100

    Index page and owl carousel

--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(document).ready(function () {
+	$(".button-collapse").sideNav();
+
+	$(".owl-carousel").owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		autoplayTimeout: 5000,
+		autoplayHoverPause: true
+	});
+});
