a07e1ccd8150cdc60086e830d1f3aa56c8f46c95
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2016-03-20 17:36:59 +0100

    MVC4 initial

---
+++ Termoservis/Termoservis.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Termoservis.Web.Startup))]
+namespace Termoservis.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
